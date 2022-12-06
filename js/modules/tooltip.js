18d11b370c2978487c0052528daa5800d3a312aa
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2022-12-06 00:01:20 -0300

    tooltip no mapa feito

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -3,6 +3,8 @@ export default class ToolTip {
     this.tooltip = document.querySelector(tooltip);
 
     this.mouseEntrou = this.mouseEntrou.bind(this);
+    this.mouseMoveu = this.mouseMoveu.bind(this);
+    this.mouseSaiu = this.mouseSaiu.bind(this);
   }
 
   criarToolTipBox(e) {
@@ -10,11 +12,30 @@ export default class ToolTip {
     const text = e.getAttribute('aria-label');
     tooltipBox.innerText = text;
     tooltipBox.classList.add('tooltip');
-    console.log(tooltipBox)
+    document.body.appendChild(tooltipBox);
+    this.tooltipBox = tooltipBox;
+  }
+
+  mouseSaiu(e) {
+    this.tooltipBox.remove();
+    e.currentTarget.removeEventListener('mouseleave', this.mouseSaiu);
+    e.currentTarget.removeEventListener('mousemove', this.mouseMoveu);
+  }
+
+  mouseMoveu(e) {
+    this.tooltipBox.style.top = `${e.pageY + 20}px`;
+    if (e.pageX + 240 > window.innerWidth) {
+      this.tooltipBox.style.left = `${e.pageX - 200}px`;
+    } else {
+      this.tooltipBox.style.left = `${e.pageX + 20}px`;
+    }
   }
 
   mouseEntrou(e) {
     this.criarToolTipBox(e.currentTarget);
+
+    e.currentTarget.addEventListener('mouseleave', this.mouseSaiu);
+    e.currentTarget.addEventListener('mousemove', this.mouseMoveu);
   }
 
   event() {
